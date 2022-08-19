1cc552131ea1e4c82bb31444907182c6ceced0fc
Anjubemi <antony77_af@hotmail.com>
2022-08-18 23:35:22 -0500

    Proyecto Inicial

---
+++ src/main/java/com/idat/MicroservicioPizza/EfAntonyBecerraMicroservicioPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MicroservicioPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfAntonyBecerraMicroservicioPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAntonyBecerraMicroservicioPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/MicroservicioPizza/EfAntonyBecerraMicroservicioPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MicroservicioPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfAntonyBecerraMicroservicioPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
